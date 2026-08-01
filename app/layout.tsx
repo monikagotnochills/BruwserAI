e98599ce91738671ec9de92f9bbf8aa8cb57e784
Monika Sharma <ms3319341@gmail.com>
2026-08-01 15:59:25 +0530

    added clerk orgs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
-import {
-  ClerkProvider,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs"
+import { ClerkProvider } from "@clerk/nextjs"
 import { shadcn } from "@clerk/ui/themes"
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "@clerk/ui/themes/shadcn.css"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Button } from "@/components/ui/button"
 import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { cn } from "@/lib/utils"
@@ -46,21 +39,6 @@ export default function RootLayout({
         >
           <ThemeProvider>
             <TooltipProvider>
-              <header className="flex h-14 items-center justify-end gap-3 border-b px-6">
-                <Show when="signed-out">
-                  <SignInButton>
-                    <Button variant="ghost" size="sm">
-                      Sign in
-                    </Button>
-                  </SignInButton>
-                  <SignUpButton>
-                    <Button size="sm">Sign up</Button>
-                  </SignUpButton>
-                </Show>
-                <Show when="signed-in">
-                  <UserButton />
-                </Show>
-              </header>
               {children}
               <Toaster />
             </TooltipProvider>
